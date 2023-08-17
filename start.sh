2ffbd50667bed2605657736ff6f7813eafc150e5
Henry Ford <freaktownmail@gmail.com>
2023-08-17 13:48:54 +0200

    Linted start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 s=/mnt/vrising/server
 p=/mnt/vrising/persistentdata
 echo "Setting timezone to $TZ"
-echo $TZ > /etc/timezone 2>&1
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 2>&1
+echo "$TZ" > /etc/timezone 2>&1
+ln -snf /usr/share/zoneinfo/"$TZ" /etc/localtime 2>&1
 dpkg-reconfigure -f noninteractive tzdata 2>&1
 
 #What is this supposed to do?
@@ -14,11 +14,11 @@ if [ -z "$WORLDNAME" ]; then
 	WORLDNAME="world1"
 fi
 game_port=""
-if [ ! -z $GAMEPORT ]; then
+if [ -n "$GAMEPORT" ]; then
 	game_port=" -gamePort $GAMEPORT"
 fi
 query_port=""
-if [ ! -z $QUERYPORT ]; then
+if [ -n "$QUERYPORT" ]; then
 	query_port=" -queryPort $QUERYPORT"
 fi
 mkdir -p /root/.steam 2>/dev/null
@@ -27,7 +27,7 @@ echo " "
 echo "Updating V-Rising Dedicated Server files..."
 echo " "
 /usr/bin/steamcmd +force_install_dir "$s" +login anonymous +app_update 1829350 +quit
-echo "steam_appid: "`cat $s/steam_appid.txt`
+echo "steam_appid: $(cat $s/steam_appid.txt)"
 echo " "
 mkdir "$p/Settings" 2>/dev/null
 if [ ! -f "$p/Settings/ServerGameSettings.json" ]; then
@@ -38,7 +38,7 @@ if [ ! -f "$p/Settings/ServerHostSettings.json" ]; then
         echo "$p/Settings/ServerHostSettings.json not found. Copying default file."
         cp "$s/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json" "$p/Settings/" 2>&1
 fi
-cd "$s"
+cd "$s" || return
 echo "Starting V Rising Dedicated Server with name $SERVERNAME"
 echo "Trying to remove /tmp/.X0-lock"
 rm /tmp/.X0-lock 2>&1
